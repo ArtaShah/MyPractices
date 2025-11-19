5b2ba13e49b7056c9ef411c7c491f6eb7c05e2d9
ArtaShah <shahhoseini1717@gmail.com>
2025-11-20 02:52:27 +0330

    Add src files

---
+++ src/Number of Substrings With Only 1s.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numSub(self, s: str) -> int:
+        mod = 10**9 + 7
+        count = 0
+        result = 0
+        for ch in s:
+            if ch == '1':
+                count += 1
+            else:
+                result += count * (count + 1) // 2
+                count = 0
+        result += count * (count + 1) // 2
+        return result % mod
