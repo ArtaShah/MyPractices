47dc4ebd13e04736ae9e7626bd9e9806e76e909d
Arta Shah <shahhoseini1717@gmail.com>
2025-12-29 02:48:00 +0330

    src files

--- src/Reverse nodes in kgroup/main.ts
+++ src/Reverse nodes in kgroup/main.ts
@@ -10,24 +10,24 @@ function reverseKGroup(head: ListNode | null, k: number): ListNode | null {
     const dummy = new ListNode(0);
     dummy.next = head;
     let groupPrev: ListNode | null = dummy;
-  while (true) {
-    let kth = groupPrev;
-    for (let i = 0; i < k && kth; i++) {
-      kth = kth.next;
+    while (true) {
+        let kth = groupPrev;
+        for (let i = 0; i < k && kth; i++) {
+          kth = kth.next;
+        }
+        if (!kth) break;
+        const groupNext = kth.next;
+        let prev = groupNext;
+        let curr = groupPrev!.next;
+        while (curr !== groupNext) {
+            const tmp = curr!.next;
+            curr!.next = prev;
+            prev = curr!;
+            curr = tmp;
+        }
+        const tmp: ListNode | null = groupPrev!.next;
+        groupPrev!.next = kth;
+        groupPrev = tmp;
     }
-    if (!kth) break;
-    const groupNext = kth.next;
-    let prev = groupNext;
-    let curr = groupPrev!.next;
-    while (curr !== groupNext) {
-      const tmp = curr!.next;
-      curr!.next = prev;
-      prev = curr!;
-      curr = tmp;
-    }
-    const tmp: ListNode | null = groupPrev!.next;
-    groupPrev!.next = kth;
-    groupPrev = tmp;
-  }
-  return dummy.next;
+    return dummy.next;
 };
