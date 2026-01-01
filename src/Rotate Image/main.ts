e5bece53ed449be094b6bf5c78293bdca50b0eeb
Arta Shah <shahhoseini1717@gmail.com>
2026-01-02 03:07:33 +0330

    src files

---
+++ src/Rotate Image/main.ts
@@ -0,0 +1,12 @@
+function rotate(matrix: number[][]): void {
+    const rotatedMatrix=new Array<number>(matrix.length);
+    const n=matrix.length;
+    for(let i=0;i<n;i++){
+        for(let j=i+1;j<n;j++){
+            [matrix[i][j],matrix[j][i]]=[matrix[j][i],matrix[i][j]];
+        }
+    }
+    for(let i=0;i<n;i++){
+        matrix[i].reverse();
+    }
+};
